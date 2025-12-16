a5cdd5fe3dd633a9dd4bd179c9ddbe5968146bbd
maestitia-x <maestitia0@gmail.com>
2025-12-16 03:02:39 +0300

    First Commit

--- app/views.py
+++ app/views.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post, Comment, Category
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Home Page')
+    # Tüm ** yayınlanmış ** postları veritabanından çek(published=True olanlar)
+    # - Tüm kategorileri çek - Bunları context dictionary 'sine ekle - Template 'e gönder
+    posts = Post.objects.filter(published = True)
+    categories = Category.objects.all()
+    context = {
+        'categories' : categories,
+        'posts' : posts,
+
+    }
+    return render(request, 'app/home.html', context)
 
 def posts(request):
     return HttpResponse('Posts Page')
