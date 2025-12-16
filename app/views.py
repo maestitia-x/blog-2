9e12badc1230b4ae3105d30c97c54d9e062cfce0
maestitia-x <maestitia0@gmail.com>
2025-12-16 04:26:38 +0300

    'third-commit'

--- app/views.py
+++ app/views.py
@@ -16,15 +16,21 @@ def home(request):
     return render(request, 'app/home.html', context)
 
 def posts(request):
-    return HttpResponse('Posts Page')
+    # Tüm yayınlanmış postları çek Template 'e gönder
+    posts = Post.objects.filter(published=True)
+    context = {
+        'posts':posts
+    }
+    return render(request, 'app/posts.html', context)
 
 def post(request, slug):
     #
     # URL'den gelen slug parametresini kullanarak ilgili postu bul Eğer post bulunamazsa 404 hatası döndür
     # Her ziyarette views sayısını 1 artır ve kaydet
     # Post 'un aktif yorumlarını çek (active=True) Bunları context 'e ekle ve template' e gönder
-    post_obj = get_object_or_404(Post)
+    post_obj = get_object_or_404(Post, slug=slug)
     post_obj.views += 1
+    post_obj.save()
     comments = post_obj.comments.filter(active=True)
     context={
         'post':post_obj,
@@ -33,7 +39,17 @@ def post(request, slug):
     return render(request, 'app/post.html', context)
 
 def category(request, id):
-    return HttpResponse("Category Page")
+    # URL'den gelen id ile kategoriyi bul (404 kontrolü)
+    category_obj = get_object_or_404(Category, id)
+    # O kategoriye ait yayınlanmış postları çek
+    posts = category_obj.posts.filter(published=True)
+    # Context'e kategori ve postları ekle
+
+    context={
+        'category_obj': category_obj,
+        'posts':posts
+    }
+    return render(request, 'app/category.html', context)
 
 def profile(request):
     return HttpResponse('Profile Page')
